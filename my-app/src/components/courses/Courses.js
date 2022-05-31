40ffa58051d7598e38d3ef6cd8089773af1680a7
eoinsheehan <31827583+eoinsheehan@users.noreply.github.com>
2022-05-31 21:59:35 +0100

    Add checkboxes as semester filter

--- my-app/src/components/courses/Courses.js
+++ my-app/src/components/courses/Courses.js
@@ -1,9 +1,16 @@
 import CourseHeading from './CourseHeading'
 import Card from '../UI/Card'
+import { useState } from 'react'
 import './Courses.css'
 
 const Courses = (props) => {
 
+    const [isChecked,setIsChecked] = useState({"autumn":true,"spring":true});
+
+    const handleChange = (e)=> {
+        setIsChecked({...isChecked,[e.target.name]:e.target.checked})
+    }
+    
     let content;
 
     if(props.testing.length >0){
@@ -12,9 +19,15 @@ const Courses = (props) => {
     }
 
     return (
+        <div>
+        <input onChange = {handleChange} type="checkbox" id="vehicle1" name="autumn" value="autumn" checked={isChecked["autumn"]}/>
+        <label for="autumn"> Autumn</label>
+        <input onChange = {handleChange} type="checkbox" id="vehicle2" name="spring" value="spring" checked={isChecked["spring"]}/>
+        <label for="spring"> Spring</label>
         <Card className ="courses">
         {content}
         </Card>
+        </div>
     )
 }
 
