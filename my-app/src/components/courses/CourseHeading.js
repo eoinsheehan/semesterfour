87b07cc34361c49c40f494325235d7fd7862306b
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-06-10 21:46:58 +0100

    Add styling to dropdowns

--- my-app/src/components/courses/CourseHeading.js
+++ my-app/src/components/courses/CourseHeading.js
@@ -26,15 +26,20 @@ const CourseHeading = (props) =>{
 
 
     // iterable used in map will contain all module details acessed via slicing
-    let helpme = semesterFilter.map(course =><CourseList onSelect={props.onSelect} title ={course.title} coursesSelected={props.coursesSelected} details = {course.details}/> );
+    let helpme = semesterFilter.map(course =><CourseList id= {course.title} className="accordion-collapse collapse" onSelect={props.onSelect} title ={course.title} coursesSelected={props.coursesSelected} details = {course.details}/> );
 
     return (
-        <div>
-        <div className="course-heading">{props.title}
-        <button onClick={toggleCourses}>Expand</button>
-        </div>
+
+        <div className="accordion-header">
+        {/* <div className="course-heading">{props.title}
+        <button >Expand</button> */}
+        <button onClick={toggleCourses}  className={showContent ? "accordion-button collapsed" : "accordion-button"} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        {props.title}
+      </button>
+        {/* </div> */}
         {showContent ? helpme : null}
         </div>
+
     )
 }
 export default CourseHeading
