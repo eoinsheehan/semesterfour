20ce617e012bc841aedca2b3403dcfd4e66efe36
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-06-23 14:58:10 +0100

    Add npm install to deployment workflow

--- my-app/src/components/cart/Cart.js
+++ my-app/src/components/cart/Cart.js
@@ -29,7 +29,7 @@ const Cart = (props) => {
         <CartItem onRemove = {props.onRemove} title = {course.title} credits = {course.credits}/>)
     return(
         <Card className="cart">
-            <h2>Modules Chosen</h2>
+            <h2>Modules Choices</h2>
             {cartContents}
             {creditCount}
         </Card>
