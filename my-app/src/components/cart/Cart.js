ce861f76850a7f312416f9211de224329990aa9a
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-06-23 15:17:00 +0100

    Update module selections component heading

--- my-app/src/components/cart/Cart.js
+++ my-app/src/components/cart/Cart.js
@@ -29,7 +29,7 @@ const Cart = (props) => {
         <CartItem onRemove = {props.onRemove} title = {course.title} credits = {course.credits}/>)
     return(
         <Card className="cart">
-            <h2>Module Selection</h2>
+            <h2>Module Selections</h2>
             {cartContents}
             {creditCount}
         </Card>
