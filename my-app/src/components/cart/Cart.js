a9fa1a2a9937ad40316221442eece8a01ba104c4
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-12-24 19:17:02 +0000

    Add progress bar for module selection

--- my-app/src/components/cart/Cart.js
+++ my-app/src/components/cart/Cart.js
@@ -24,24 +24,33 @@ const Cart = (props) => {
         return course['theme'] ==="Software Engineering"});
         console.log("filtered",filteredCourses)
     }
-
+    let sum=0;
     if(props.selection.length>0){
-        let sum = props.selection.reduce(
+        sum = props.selection.reduce(
             (previousValue, currentValue) => previousValue + currentValue.credits,
             initialValue
         )
         creditCount = "Number of credits: " + sum
     }
 
+    console.log("credit count",sum/30);
+    let dynamicQuantity = (sum/30)*100+"%";
+
+    console.log("dynamic",dynamicQuantity);
+
     let cartContents = props.selection.map(course => 
         <CartItem onRemove = {props.onRemove} title = {course.title} credits = {course.credits}/>)
     return(
-        <div className='text-black px-8 py-6 top-1 sticky'>
-            <p className='text-5xl'>Module Selection</p>
+        <div className='text-black px-8 py-6 top-1 sticky flex flex-col'>
+            <div><p className='text-5xl'>Module Selection</p>
             {cartContents}
             <p className="text-2xl font-bold">
             {creditCount}
             </p>
+            </div>
+            <div className='bg-white h-4 w-full border-2 border-black rounded-full'>
+                <div style={{width: dynamicQuantity}} className={`bg-green-500 h-3 rounded-full max-w-full`}></div>
+            </div>
         </div>
     )
 }
